a012c519790205ad861b40a59a6d7d1bfa8e1bae
Matt Warren <mwarren@harding.edu>
2016-08-17 17:31:17 -0400

    implement play_game method.

--- day-3/game_of_rooms/rooms.rb
+++ day-3/game_of_rooms/rooms.rb
@@ -1,18 +1,11 @@
-require_relative 'game.rb'
-
 class Room
+	attr_reader :description, :exit
 	def initialize(description)
 		@description = description
 		@exit = "Exits: [ back ]"
 	end
 end
 
-main = Room.new("You are facing north in a room with 4 doors. Pick a door. [ N, S, W, E ] type [ exit ] to quit.")
-north = Room.new("Now you're in the green room. There are tomato plants growing. What do you want to do to the tomatoes? [ stomp, pick, throw ]")
-south = Room.new("You found the janitor's closet. Just a bunch of mops and brooms.")
-east = Room.new("As you walk in, the light from the windows blinds your eyes. There's a sledgehammer nearby. What do you want to do to the windows? [ shut, shatter ]")
-west = Room.new("You entered the sky room. You are standing on a cloud. You see a blue sky all around you. Don't fall.")
 
-rooms = [ north, south, east, west ]
-new_game = Game.new(rooms)
+
 
