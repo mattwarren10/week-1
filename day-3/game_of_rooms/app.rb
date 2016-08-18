09bf2978b42e561551cab2b99c66f06ecde42593
Matt Warren <mwarren@harding.edu>
2016-08-18 09:06:26 -0400

    mv instantiations and driver_code to app.rb

--- day-3/game_of_rooms/app.rb
+++ day-3/game_of_rooms/app.rb
@@ -1,6 +1,19 @@
-require_relative 'game.rb'
+require_relative("rooms.rb")
+require_relative("game.rb")
+
+main = Room.new("You are facing north in a room with 4 doors. Pick a door. [ N, S, W, E ] type [ exit ] to quit.")
+north = Room.new("Now you're in the green room. There are tomato plants growing. What do you want to do to the tomatoes? [ stomp, pick, throw ]")
+east = Room.new("As you walk in, the light from the windows blinds your eyes. There's a sledgehammer nearby. What do you want to do to the windows? [ shut, shatter ]")
+south = Room.new("You found the janitor's closet. Just a bunch of mops and brooms.")
+west = Room.new("You entered the sky room. You are standing on a cloud. You see a blue sky all around you. Don't fall.")
+
+rooms = [ main, north, east, south, west ]
+
+input = ""
 while (input != "exit")
-	puts @location.description
+	binding.pry
+	new_game = Game.new(rooms)
+	puts @location
 	puts "Where do you want to go?"
 	print "> "
 	input = gets.chomp
