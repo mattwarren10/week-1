6045b974bf8503fff607804c451e6516e9d5ae6e
Matt Warren <mwarren@harding.edu>
2016-08-19 09:03:27 -0400

    finalize game_of_rooms

--- day-3/game_of_rooms/app.rb
+++ day-3/game_of_rooms/app.rb
@@ -6,7 +6,7 @@ main = Room.new("You are facing north in a room with 4 doors. Pick a door. [ n, 
 north = Room.new("Now you're in the green room. There are tomato plants growing. What do you want to do to the tomatoes? [ stomp, pick, throw ]", { :stomp => "'Arrrggh,' you say. Now the ground is all slimy.", :pick => "You take a bite. Eww, it's gross.", :throw => "Smash!! The tomato has been destroyed." })
 east = Room.new("As you walk in, the light from the windows blinds your eyes. There's a sledgehammer nearby. What do you want to do to the windows? [ shut, shatter ]", { :shut => "You shut all the windows.", :shatter => "All the windows are shattered. Now you feel a nice breeze." })
 south = Room.new("You found the janitor's closet. Just a bunch of mops and brooms. Type [ steal ] to take the mop.", { :steal => "You have a mop. Now go clean the floors!"})
-west = Room.new("You entered the sky room. You are standing on a cloud. You see a blue sky all around you. Their bouncy like trampolines. [ jump ]", { :jump => "Very bouncy!" })
+west = Room.new("You entered the sky room. You are standing on a cloud. You see a blue sky all around you. They're bouncy like trampolines. [ jump ]", { :jump => "Very bouncy!" })
 
 rooms = [ north, east, south, west ]
 
--- day-3/game_of_rooms/lib/rooms.rb
+++ day-3/game_of_rooms/lib/rooms.rb
@@ -2,7 +2,7 @@ class Room
 	attr_reader :description, :exit, :objects
 	def initialize(description, objects)
 		@description = description
-		@exit = "Exits: [ back ]"
+		@exit = "Exits: Type [ back ]"
 		@objects = objects
 	end
 
