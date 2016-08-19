3c2097a610cde9e5dbc68d01d8cf480215203e07
Matt Warren <mwarren@harding.edu>
2016-08-19 14:47:10 -0400

    rm redundant if statements

--- day-5/chess_validator/lib/bishop.rb
+++ day-5/chess_validator/lib/bishop.rb
@@ -7,20 +7,8 @@ class Bishop < Piece
   end
 
   def can_move?(final_x, final_y)
-  	if (final_x - @x).abs == 1 && (final_y - @y).abs == 1 
+  	if ((final_x - @x).abs - (final_y - @y).abs).abs == 0
   		"yes"
-  	elsif (final_x - @x).abs == 2 && (final_y - @y).abs == 2
-      "yes"
-    elsif (final_x - @x).abs == 3 && (final_y - @y).abs == 3
-      "yes"
-    elsif (final_x - @x).abs == 4 && (final_y - @y).abs == 4
-      "yes"
-    elsif (final_x - @x).abs == 5 && (final_y - @y).abs == 5
-      "yes"
-    elsif (final_x - @x).abs == 6 && (final_y - @y).abs == 6
-      "yes"
-    elsif (final_x - @x).abs == 7 && (final_y - @y).abs == 7
-      "yes"
     else
   		"no"
   	end
