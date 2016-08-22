3320b5286390a8892f87dbf64a875ad7cc76ac4a
Matt Warren <mwarren@harding.edu>
2016-08-22 09:37:06 -0400

    clean up king.rb can_move? method

--- day-5/chess_validator/lib/king.rb
+++ day-5/chess_validator/lib/king.rb
@@ -7,18 +7,12 @@ class King < Piece
   end
 
   def can_move?(final_x, final_y)
-  	if (final_x - @x).abs == 1 && (final_y - @y).abs == 1
+  	if ((final_x - @x).abs - (final_y - @y).abs) == 0
   		"yes"
-  	elsif (final_x - @x).abs == 1 && (final_y - @y).abs == 0
+  	elsif ((final_x - @x).abs - (final_y - @y).abs) == 1
   		"yes"
-  	elsif (final_x - @x).abs == 0 && (final_y - @y).abs == 1
-  		"yes"
-  	elsif (final_x - @x).abs == 2 || (final_y - @y).abs == 2
-  		"no"
-  	elsif (final_x - @x).abs == 2 || (final_y - @y).abs == 1
-  		"no"
-  	elsif (final_x - @x).abs == 1 || (final_y - @y).abs == 2
-  		"no"
+  	else 
+      "no"
   	end
   end
 
